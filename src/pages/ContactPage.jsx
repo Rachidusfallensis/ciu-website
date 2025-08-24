8d7d071c9d550c975e5244aee55ed87171a0e400
Rachidusfallensis <rachidfall5@gmail.com>
2025-08-24 07:46:02 +0200

    fix: Correction la position de l'icône Send dans le bouton Envoyer le Message

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -188,7 +188,7 @@ export default function ContactPage() {
                       Message *
                     </label>
                     <div className="relative">
-                      <MessageSquare className="absolute left-3 top-4 h-5 w-5 text-gray-400" />
+                      <MessageSquare className="absolute left-3 top-3 h-5 w-5 text-gray-400" />
                       <textarea
                         id="message"
                         name="message"
@@ -236,10 +236,10 @@ export default function ContactPage() {
                         Envoi en cours...
                       </>
                     ) : (
-                      <>
+                      <div className="flex items-center justify-center">
                         <Send className="mr-2 h-5 w-5" />
                         Envoyer le Message
-                      </>
+                      </div>
                     )}
                   </motion.button>
                 </form>
