1e728f265096672a5a600f3c18c8f575a10e72e8
Rachidusfallensis <rachidfall5@gmail.com>
2026-02-07 09:50:46 +0100

    Updates

---
+++ assets/maps-yTm4ykrC.js
@@ -0,0 +1,1 @@
+import"./router-DL8YiHNL.js";
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ciu-website/sw.js', { scope: '/ciu-website/' })})}
